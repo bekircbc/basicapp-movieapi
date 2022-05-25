ce620f384a0cabd1d70c98c069c96d941209b4a8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-25 16:21:41 +0200

    movies added to page.. but api not working.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,20 @@ import { AppContext } from "./AppContext";
 function App() {
   const { movies } = useContext(AppContext);
   console.log(movies);
-  return <div className="App">Hello</div>;
+
+  return (
+    <div className="App">
+      <div className="movies">
+        {movies.map((m, i) => {
+          return (
+            <ul>
+              <li key={i}>{m.title}</li>
+            </ul>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
