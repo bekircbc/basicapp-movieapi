8a38aee6f6061493d1f4e46fb8dab7e864712bd8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-25 16:30:34 +0200

    api not working

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -3,18 +3,8 @@ import axios from "axios";
 
 export const AppContext = createContext();
 
-// const moviesUrl =
-//   "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup";
-
-const options = {
-  url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup",
-  params: { term: "bojack", country: "uk" },
-  headers: {
-    "X-RapidAPI-Host":
-      "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-  },
-};
+const moviesUrl =
+  "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup";
 
 export const AppProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
