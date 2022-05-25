01aacdc9e5880b44cbcb76b509111301471f4479
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-25 16:04:49 +0200

    AppContext.jsx added for getting movies and sending to child components.

---
+++ src/AppContext.jsx
@@ -0,0 +1,29 @@
+import { createContext } from "react-dom";
+import axios from "axios";
+
+export const AppContext = createContext();
+
+const moviesUrl =
+  "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup";
+
+export const AppProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setMovies((await axios.get(moviesUrl)).data);
+    })();
+  }, []);
+
+  return (
+    <>
+      <AppProvider
+        value={{
+          movies,
+        }}
+      >
+        {children}
+      </AppProvider>
+    </>
+  );
+};
