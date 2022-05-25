5e2d9775d934c9ed6e61b198f973e33ba285fb12
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-25 16:24:24 +0200

    added object options for getting api

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -3,15 +3,25 @@ import axios from "axios";
 
 export const AppContext = createContext();
 
-const moviesUrl =
-  "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup";
+// const moviesUrl =
+//   "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup";
+
+const options = {
+  url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup",
+  params: { term: "bojack", country: "uk" },
+  headers: {
+    "X-RapidAPI-Host":
+      "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+  },
+};
 
 export const AppProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     (async () => {
-      setMovies((await axios.get(moviesUrl)).data);
+      setMovies((await axios.get(options)).data);
     })();
   }, []);
 
