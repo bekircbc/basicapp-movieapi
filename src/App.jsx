96c14adab052ee373b47724e8f1d7097d20c5e20
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-25 16:14:20 +0200

    movies added to App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.scss";
+import { useContext } from "react";
+import { AppContext } from "./AppContext";
 
 function App() {
+  const { movies } = useContext(AppContext);
+  console.log(movies);
   return <div className="App">Hello</div>;
 }
 
